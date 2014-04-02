ca13019fc4cfabc3f310b2a4dadb4f4efea36ece
JGJeffryes <JGJeffryes@1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.ip6.arpa>
2014-04-02 13:03:51 -0500

    added molfile coordinate calculation

--- lib/biokbase/mine_database/Impl.py
+++ lib/biokbase/mine_database/Impl.py
@@ -146,6 +146,7 @@ class mineDatabaseServices:
             meh = db.compounds.find_one({'_id': x})
             if not 'Molfile' in meh:
                 mol = pybel.readstring('smi', str(meh['SMILES']))
+                mol.draw(show=False, update=True)
                 meh['Molfile'] = mol.write('mol')
             objects.append(meh)
         #END get_comps
