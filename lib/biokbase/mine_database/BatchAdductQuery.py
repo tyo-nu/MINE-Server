45640c17a769414fdd51a1dd637756c1f1d26b90
JGJeffryes <JGJeffryes@cheme-85.chem-eng.northwestern.edu>
2014-10-08 11:05:39 -0500

    adjusted adduct file

--- lib/biokbase/mine_database/BatchAdductQuery.py
+++ lib/biokbase/mine_database/BatchAdductQuery.py
@@ -82,6 +82,8 @@ class Dataset():
                 query_terms +=[{"logP": {"$gte": self.min_logP}}, {"logP": {"$lte": self.max_logP}}]
             if hasattr(self, 'min_kovats'):
                 query_terms += [{"maxKovatsRI": {"$gte": self.min_kovats}}, {"minKovatsRI": {"$lte": self.max_kovats}}]
+            if adduct['f0'] == 'M+':
+                pass
             hits[adduct['f0']] = [x for x in db.compounds.find({"$and": query_terms}, {'Formula': 1,'MINE_id': 1,
                                                     'Names': 1, 'SMILES': 1, 'Inchikey': 1, 'steps_from_source': 1})]
 
