d414665129e51f9e285d8d98f701aab0f80f089c
Jon Strutz <jonstrutz11@gmail.com>
2020-05-06 11:46:16 -0500

    added mock rdkit for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,3 +54,6 @@ html_theme = 'classic'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+# Can't install C binaries on readthedocs.org
+autodoc_mock_imports = ["rdkit"]
